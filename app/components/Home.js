7a11becdae80289783258c01ede5b1c57962b98d
stevenjmarsh <sjm.atl@gmail.com>
2016-09-30 18:13:09 -0400

    adding simple containers and adjusting some eslint rules

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { Link } from "react-router";
+
+import { transparentBg } from "../styles";
 
 const Home = () => {
   return (
-    <div>
-      Home component.
+    <div className="jumbotron col-sm-12 text-center" style={transparentBg}>
+      <h1>Github Battle</h1>
+      <p className="lead">What even is a jQuery?</p>
+      <Link to="/playerOne">
+        <button type="button" className="btn btn-lg btn-success">Get Started</button>
+      </Link>
     </div>
   );
 };
--- app/components/Main.js
+++ app/components/Main.js
@@ -3,8 +3,7 @@ import React, { Component } from "react";
 class Main extends Component {
   render() {
     return (
-      <div>
-        Main component.
+      <div className="main-container">
         {this.props.children}
       </div>
     );
