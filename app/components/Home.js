2c3efdb2e3da14c198dee8b6b26929c7a15d64b6
stevenjmarsh <sjm.atl@gmail.com>
2016-10-04 15:30:43 -0400

    removing unneeded components, to keep boilerplate simple / light

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router";
 
 import { transparentBg } from "../styles";
 
@@ -8,9 +7,6 @@ const Home = () => {
     <div className="jumbotron col-sm-12 text-center" style={transparentBg}>
       <h1>Hello React World!</h1>
       <p className="lead">A very simple react boilerplate.</p>
-      <Link to="/samplePage">
-        <button type="button" className="btn btn-lg btn-success">Sample Page</button>
-      </Link>
     </div>
   );
 };
--- app/config/routes.js
+++ app/config/routes.js
@@ -3,13 +3,11 @@ import { Router, Route, IndexRoute, hashHistory } from "react-router";
 
 import Main from "../components/Main";
 import Home from "../components/Home";
-import PromptContainer from "../containers/PromptContainer";
 
 const routes = (
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
       <IndexRoute component={Home} />
-      <Route path="samplePage" component={PromptContainer} header="Sample Page" />
     </Route>
   </Router>
 );
